a3d90b868ece20c8ca0c19041d9b2920e7b55db6
Douglas Mueller <douglasmueller6@gmail.com>
2019-10-28 18:26:49 -0400

    Word Guess version 1.1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
-javascript file
+
+
+
+var presidentLastName = [
+    "Trump",
+    "Obama",
+    "Clinton",
+    "Reagan",
+    "Carter",
+    "Ford",
+    "Nixon",
+    "Johnson",
+    "Kennedy",
+    "Eisenhower"
+];
+
+
+var answerArray = [];
+for (var i = 0; i < presidentLastName.length; i++) {
+ answerArray[i] = " _ ";
+}
+function myFunction() {
+    document.getElementById("openingStatement").innerHTML = "Click any button to start";
+    }
+
+console.log (answerArray);
+
+document.onkeyup = function(event) {
+    var letterGuess = event.key.toLowerCase();
+}
+console.log(letterGuess);
+
+// var letterGuess = event.key.toLowerCase();
+
+// var chosenWord = presidentLastName[Math.floor(Math.random()
+//      * presidentLastName.length)];
+// console.log (chosenWord);
+
+// function myFunction() {
+// document.getElementById("openingStatement").innerHTML = voteable;
+// }
