6fd27b3036b539c378f101e93d47221fec5059c7
Douglas Mueller <douglasmueller6@gmail.com>
2019-10-29 23:52:13 -0400

    revision 1.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-
-
-
+//Variables defined
 var presidentLastName = [
     "Trump",
     "Obama",
@@ -13,29 +11,61 @@ var presidentLastName = [
     "Kennedy",
     "Eisenhower"
 ];
+var userGuess = [];
+var guessesLeft = 10;
+var userGuesses = [];
+var wrongLetter = [];
+var underScores = [];
 
+// function startGame(){
+//chose random word from array
 
+var chosenWord = presidentLastName[Math.floor(Math.random() * presidentLastName.length)];
+console.log (chosenWord);
+
+//create blank array with "_"
 var answerArray = [];
-for (var i = 0; i < presidentLastName.length; i++) {
- answerArray[i] = " _ ";
+for (var i = 0; i < chosenWord.length; i++) {
+   
+underScores.push("_");
 }
-function myFunction() {
-    document.getElementById("openingStatement").innerHTML = "Click any button to start";
+
+
+//start game, run until guessesleft=0
+
+console.log(underScores)
+//capture user typed letter
+document.onkeyup = function(event)
+{
+    userGuess=event.key;
+    console.log(userGuess)
+
+
+console.log(guessesLeft)
+//compare guessed letter to chosenWord letters
+if (chosenWord.indexOf(userGuess) > -1)
+{
+    for(var j=0; j<chosenWord.length; j++)
+    {
+        if(chosenWord[j]=== userGuess)
+            {
+        //push user letter guessed to underscores index
+        underScores[j] = userGuess;
+        console.log(underScores);
     }
 
-console.log (answerArray);
-
-document.onkeyup = function(event) {
-    var letterGuess = event.key.toLowerCase();
+        else{
+    //push incorrect letter to wrongLetter array
+     wrongLetter.push(userGuess);
+    //reduce user guesses by 1
+    userGuesses--;
+  }
 }
-console.log(letterGuess);
+} 
 
-// var letterGuess = event.key.toLowerCase();
+    
 
-// var chosenWord = presidentLastName[Math.floor(Math.random()
-//      * presidentLastName.length)];
-// console.log (chosenWord);
-
-// function myFunction() {
-// document.getElementById("openingStatement").innerHTML = voteable;
-// }
+console.log(wrongLetter);
+console.log(userGuesses);
+console.log(underScores);
+}
