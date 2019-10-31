9feb6af545137746207ee2f292fb9424b6cd36fb
Douglas Mueller <douglasmueller6@gmail.com>
2019-10-30 23:53:22 -0400

    version 1.6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+ $(document).ready(function() {
 
 //Variables defined
 var presidentLastName = [
@@ -33,85 +33,69 @@ var userGuess = [];
 var wrongLetter = [];
 //counter for correct letters in the chosenWord
 var correctLetter = 0;
+var correctGuessCounter = 0;
 
 //Game stats
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 10;
 
-//functions
-//function that starts a new game
 
-function startGame(){
+//function that starts a new game
+function startGame() {
+
     //chose random word from array
 
 var chosenWord = presidentLastName[Math.floor(Math.random() * presidentLastName.length)];
 console.log ("The current chosen word is: " + chosenWord);
-//grab chosen word and break it into individual letters
-chosenWordLetters = chosenWord.split("");
-console.log ("The chosen word letters are: " + chosenWordLetters);
-//grab chosen word and get number of letters in it
-numberBlanks = chosenWordLetters.length;
-console.log ("The number of letters in chosen word is: " + numberBlanks);
-//add correct number of blanks to answerDisplay that corresponds with the length of the chosenWord
-for (i = 0; i< numberBlanks; i++) {
-    answerArray.push("_");
+for (var k=0; k<chosenWord.length; k++)
+{ 
+numberBlanks.push("_");
 }
-startGame();
-console.log(answerArray)
-function winOrLoseTheGame {
-    if (correctLetter = chosenWord.length) {
-        alert ("You win!");
-        //display president full name
-//display president photo where current hangman image is located
-//play president sound-bite
+console.log(numberBlanks);
+$("#guessesRemaining").text(guessesRemaining);
 
+
+function winOrlose(){
+    if (correctGuessCounter === chosenWord.length){
+        alert("You win!");
     }
-    else if(guessesRemaining < 1){
-        alert("You lose!")
+        else if(guessesRemaining === 0){
+        alert("You Lose!");
     }
 }
+ document.onkeyup = function(event)
+ {
+    $("#underscoredWord").text(numberBlanks);
+     userGuess=event.key;
+   
+     guessesRemaining --;
+     $("#guessesRemaining").text("Guesses remaining " + guessesRemaining);
+     console.log(guessesRemaining)
+     console.log(userGuess)
+     if(chosenWord.indexOf(userGuess) > -1){
+            for(var i=0;i < chosenWord.length; i++){
+                if(chosenWord[i] === userGuess){
+                    numberBlanks[i] =userGuess;
+                    console.log(numberBlanks);
+                    correctGuessCounter++;
+                    winOrlose();
+                    
+                }
+            }
+     }
+     else {
+         wrongLetter.push(userGuess);
+         console.log("Wrong Letter array " + wrongLetter);
+         console.log("Guesses Remaining " + guessesRemaining);
+         $("#guessedLetters").text(wrongLetter);
+         winOrlose();
 
-//create final answerDisplay of blanks
-answerDisplay=answerArray.join(" ");
-console.log(answerDisplay);
-function buttonClickToStart() {
-     
-//create loop for game play ending if guessesRemaining is zero
-for (l = 0; l>guessesRemaining; l--) {
-
-//capture user typed letter
-document.onkeyup = function(event)
-{
-    userGuess=event.key;
-    guessesRemaining -1;
-    console.log(guessesRemaining)
-    console.log(userGuess)
-
-    //check if letter already guessed incorrectly
-
-if (wrongLetter.indexOf(userGuess) === userGuess){
-    alert("You already guessed" + userGuess);
-    guessesRemaining+1;
-
-//compare guessed letter to chosenWord letters
-
-    for(var j=0; j<chosenWord.length; j++)
-    {
-        if(chosenWordLetters[j]=== userGuess)
-            {
-        //push user letter guessed to answerDisplay 
-        answerDisplay[j] = userGuess;
-        console.log(answerDisplay);
-        correctLetter++;
-        winTheGame();
-    }
-    else if{
-    //push incorrect letter to wrongLetter array
-     wrongLetter.push(userGuess);
-    console.log (wrongLetter);
-  }
-    }
+     }
+ }
 }
-}
+ 
+
+startGame ();
+ 
  });
