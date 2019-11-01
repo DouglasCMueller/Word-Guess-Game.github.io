0efc77a32f342a21c7f5a589dc265da9cd7773cf
Douglas Mueller <douglasmueller6@gmail.com>
2019-11-01 14:15:18 -0400

    version 1.8

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 
- $(document).ready(function() {
+$(document).ready(function() {
 
 //Variables defined
 var presidentLastName = [
-    "Trump",
-    "Obama",
-    "Clinton",
-    "Reagan",
-    "Carter",
-    "Ford",
-    "Nixon",
-    "Johnson",
-    "Kennedy",
-    "Eisenhower"
+    "trump",
+    "obama",
+    "clinton",
+    "reagan",
+    "carter",
+    "ford",
+    "nixon",
+    "kennedy",
+    "eisenhower",
+    "johnson" 
 ];
 //empty variable to store current word to be guessed as a string
 var chosenWord ="";
@@ -22,7 +22,6 @@ var chosenWordLetters =[];
 var numberBlanks = [];
 //variable that holds the answer in blanks form
 var answerArray = [];
-
 //variable that is actual word in blanks formatted, this will change as letters are guessed correctly
 var answerDisplay = [];
 //answerDisplay represented as a string
@@ -41,61 +40,116 @@ var losses = 0;
 var guessesRemaining = 10;
 
 
-//function that starts a new game
+//function for game start
 function startGame() {
 
-    //chose random word from array
+//chose random word from array
 
 var chosenWord = presidentLastName[Math.floor(Math.random() * presidentLastName.length)];
 console.log ("The current chosen word is: " + chosenWord);
-for (var k=0; k<chosenWord.length; k++)
-{ 
+for (var k=0; k<chosenWord.length; k++){ 
 numberBlanks.push("_");
 }
 console.log(numberBlanks);
 $("#guessesRemaining").text(guessesRemaining);
+$("#underscoredWord").text(numberBlanks);
 
-
-function winOrlose(){
+//function for win or lose
+function winOrLose(){
     if (correctGuessCounter === chosenWord.length){
-        alert("You win!");
+        if (chosenWord === "trump"){
+            $("#winningMessage").text("Congratulations from the 45th President of the United States");
+            $("#presidentName").text("Donald Trump");
+            $("#mainImage").attr("src", "assets/images/trump.jpg");
+        }
+        else if (chosenWord === "obama"){
+            $("#winningMessage").text("Congratulations from the 44th President of the United States");
+            $("#presidentName").text("Barack Obama");
+            $("#mainImage").attr("src", "assets/images/obama.jpg");
+        }
+        else if (chosenWord === "clinton"){
+            $("#winningMessage").text("Congratulations from the 42nd President of the United States");
+            $("#presidentName").text("Bill Clinton");
+            $("#mainImage").attr("src", "assets/images/clinton.jpg");
+        }
+        else if (chosenWord === "reagan"){
+            $("#winningMessage").text("Congratulations from the 40th President of the United States");
+            $("#presidentName").text("Ronald Reagan");
+            $("#mainImage").attr("src", "assets/images/reagan.jpg");
+        }
+        else if (chosenWord === "carter"){
+            $("#winningMessage").text("Congratulations from the 39th President of the United States");
+            $("#presidentName").text("Jimmy Carter");
+            $("#mainImage").attr("src", "assets/images/carter.jpg");
+        }
+        else if (chosenWord === "ford"){
+            $("#winningMessage").text("Congratulations from the 38th President of the United States");
+            $("#presidentName").text("Gerald Ford");
+            $("#mainImage").attr("src", "assets/images/ford.jpg");
+        }
+        else if (chosenWord === "nixon"){
+            $("#winningMessage").text("Congratulations from the 37th President of the United States");
+            $("#presidentName").text("Richard Nixon");
+            $("#mainImage").attr("src", "assets/images/nixon.jpg");
+        }
+        else if (chosenWord === "kennedy"){
+            $("#winningMessage").text("Congratulations from the 35th President of the United States");
+            $("#presidentName").text("John F. Kennedy");
+            $("#mainImage").attr("src", "assets/images/kennedy.jpg");
+        }
+        else if (chosenWord === "eisenhower"){
+            $("#winningMessage").text("Congratulations from the 34th President of the United States");
+            $("#presidentName").text("Dwight D. Eisenhower");
+            $("#mainImage").attr("src", "assets/images/eisenhower.jpg");
+        }
+        else if (chosenWord === "johnson"){
+            $("#winningMessage").text("Congratulations from the 36th President of the United States");
+            $("#presidentName").text("Lyndon B. Johnson");
+            $("#mainImage").attr("src", "assets/images/johnson.jpg");
+        }
     }
-        else if(guessesRemaining === 0){
-        alert("You Lose!");
+    else if (guessesRemaining <1){
+        $("#winningMessage").text("You don't know your modern presidents! -- Reload page to try again");
     }
+
 }
+//action after user makes a letter guess
  document.onkeyup = function(event)
- {
-    $("#underscoredWord").text(numberBlanks);
-     userGuess=event.key;
-   
-     guessesRemaining --;
-     $("#guessesRemaining").text("Guesses remaining " + guessesRemaining);
+{
+    userGuess=event.key;
+    guessesRemaining --;
+    $("#guessesRemaining").text(guessesRemaining);
+    // for(var p=0;p < wrongLetter.length; i++){
+    //     if(wrongLetter[i] === userGuess){
+    //         alert("You already guessed that letter!")
+    //         guessesRemaining++;
+    //     } 
+      //}
      console.log(guessesRemaining)
      console.log(userGuess)
-     if(chosenWord.indexOf(userGuess) > -1){
-            for(var i=0;i < chosenWord.length; i++){
-                if(chosenWord[i] === userGuess){
+    if(chosenWord.indexOf(userGuess) > -1)
+    {
+        for(var i=0;i < chosenWord.length; i++){
+          if(chosenWord[i] === userGuess){
                     numberBlanks[i] =userGuess;
                     console.log(numberBlanks);
-                    correctGuessCounter++;
-                    winOrlose();
-                    
-                }
-            }
-     }
-     else {
-         wrongLetter.push(userGuess);
-         console.log("Wrong Letter array " + wrongLetter);
-         console.log("Guesses Remaining " + guessesRemaining);
-         $("#guessedLetters").text(wrongLetter);
-         winOrlose();
+                    $("#underscoredWord").text(numberBlanks);
 
-     }
- }
+                    correctGuessCounter++;
+                    winOrLose();
+          } 
+        }
+    }
+    else {
+         wrongLetter.push(userGuess);
+         $("#guessedLetters").text(wrongLetter);
+         winOrLose();
+    }
+    }
+
 }
- 
+
 
 startGame ();
- 
- });
+
+});
